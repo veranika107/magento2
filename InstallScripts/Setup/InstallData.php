3422900cf0b934eb51b00a186414c2c0352a934c
veranika107 <veranika.biblis@gmail.com>
2019-08-27 17:29:37 +0300

    Task 2.1: Fix code styles

--- InstallScripts/Setup/InstallData.php
+++ InstallScripts/Setup/InstallData.php
@@ -7,48 +7,54 @@ use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 
-class InstallData implements InstallDataInterface {
+class InstallData implements InstallDataInterface
+{
 
-  const TABLE_NAME = 'magento2_first_table';
+    const TABLE_NAME = 'magento2_first_table';
 
-  public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
-    $setup->startSetup();
+    public function install(
+      ModuleDataSetupInterface $setup,
+      ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
 
-    $setup->getConnection()
-      ->insertArray(
-        $setup->getTable(self::TABLE_NAME),
-        $this->getColumns(),
-        $this->getData()
-      );
+        $setup->getConnection()
+          ->insertArray(
+            $setup->getTable(self::TABLE_NAME),
+            $this->getColumns(),
+            $this->getData()
+          );
 
-    $setup->endSetup();
-  }
+        $setup->endSetup();
+    }
 
-  /**
-   * @return array
-   */
-  private function getData() {
-    $date = gmdate('Y-m-d H:i:s');
-    return [
-      ['Petr', 'Ivanov', $date, $date],
-      ['Ivan', 'Petrov', $date, $date],
-      ['John', 'Paper', $date, $date],
-      ['Nick', 'Brown', $date, $date],
-      ['Chris', 'Black', $date, $date],
-    ];
-  }
+    /**
+     * @return array
+     */
+    private function getData()
+    {
+        $date = gmdate('Y-m-d H:i:s');
+        return [
+          ['Petr', 'Ivanov', $date, $date],
+          ['Ivan', 'Petrov', $date, $date],
+          ['John', 'Paper', $date, $date],
+          ['Nick', 'Brown', $date, $date],
+          ['Chris', 'Black', $date, $date],
+        ];
+    }
 
-  /**
-   * @return array
-   */
-  private function getColumns() {
-    return [
-      'first_name',
-      'last_name',
-      'created_at',
-      'updated_at',
-    ];
-  }
+    /**
+     * @return array
+     */
+    private function getColumns()
+    {
+        return [
+          'first_name',
+          'last_name',
+          'created_at',
+          'updated_at',
+        ];
+    }
 
 }
 
--- InstallScripts/Setup/InstallSchema.php
+++ InstallScripts/Setup/InstallSchema.php
@@ -8,71 +8,77 @@ use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
-class InstallSchema implements InstallSchemaInterface {
+class InstallSchema implements InstallSchemaInterface
+{
 
-  const TABLE_NAME = 'magento2_first_table';
+    const TABLE_NAME = 'magento2_first_table';
 
-  /**
-   * @inheritDoc
-   * @throws \Zend_Db_Exception
-   */
-  public function install(SchemaSetupInterface $setup, ModuleContextInterface $context) {
-    $setup->startSetup();
-    $this->createFirstTable($setup);
-    $setup->endSetup();
-  }
-
-  /**
-   * @param \Magento\Framework\Setup\SchemaSetupInterface $setup
-   * @return void
-   * @throws \Zend_Db_Exception
-   */
-  private function createFirstTable(SchemaSetupInterface $setup) {
-
-    if ($setup->tableExists(self::TABLE_NAME)) {
-      return;
+    /**
+     * @inheritDoc
+     * @throws \Zend_Db_Exception
+     */
+    public function install(
+      SchemaSetupInterface $setup,
+      ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
+        $this->createFirstTable($setup);
+        $setup->endSetup();
     }
 
-    $table = $setup->getConnection()
-      ->newTable($setup->getTable(self::TABLE_NAME))
-      ->addColumn(
-        'user_id',
-        Table::TYPE_SMALLINT,
-        NULL,
-        ['identity' => TRUE, 'nullable' => FALSE, 'primary' => TRUE],
-        'User ID'
-      )
-      ->addColumn(
-        'first_name',
-        Table::TYPE_TEXT,
-        255,
-        ['nullable' => FALSE],
-        'First name'
-      )
-      ->addColumn(
-        'last_name',
-        Table::TYPE_TEXT,
-        255,
-        ['nullable' => FALSE],
-        'Last name'
-      )
-      ->addColumn(
-        'created_at',
-        Table::TYPE_TIMESTAMP,
-        NULL,
-        [],
-        'Creation Time'
-      )
-      ->addColumn(
-        'updated_at',
-        Table::TYPE_TIMESTAMP,
-        NULL,
-        [],
-        'Modification Time'
-      )
-      ->setComment('First Table for Task 2');
+    /**
+     * @param   \Magento\Framework\Setup\SchemaSetupInterface  $setup
+     *
+     * @return void
+     * @throws \Zend_Db_Exception
+     */
+    private function createFirstTable(SchemaSetupInterface $setup)
+    {
 
-    $setup->getConnection()->createTable($table);
-  }
+        if ($setup->tableExists(self::TABLE_NAME)) {
+            return;
+        }
+
+        $table = $setup->getConnection()
+          ->newTable($setup->getTable(self::TABLE_NAME))
+          ->addColumn(
+            'user_id',
+            Table::TYPE_SMALLINT,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'User ID'
+          )
+          ->addColumn(
+            'first_name',
+            Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'First name'
+          )
+          ->addColumn(
+            'last_name',
+            Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Last name'
+          )
+          ->addColumn(
+            'created_at',
+            Table::TYPE_TIMESTAMP,
+            null,
+            [],
+            'Creation Time'
+          )
+          ->addColumn(
+            'updated_at',
+            Table::TYPE_TIMESTAMP,
+            null,
+            [],
+            'Modification Time'
+          )
+          ->setComment('First Table for Task 2');
+
+        $setup->getConnection()->createTable($table);
+    }
 
 }

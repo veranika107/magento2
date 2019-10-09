83f384c43a772c78434bfd2a0bcdc61909c207a3
veranika107 <veranika.biblis@gmail.com>
2019-10-09 12:36:29 +0300

    Task 2.6: Web API

---
+++ WebAPI/Api/SomeServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Epam\WebAPI\Api;
+
+interface SomeServiceInterface
+{
+
+    /**
+     * @param string $test
+     * @return mixed
+     */
+    public function someFunction(string $test);
+}
